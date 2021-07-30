2e276768bdc1b956b3cc28d9bab28e9dc521fb9f
Cristina Shim <84110240+shimy8617@users.noreply.github.com>
2021-07-30 07:45:22 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Administración | Login</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap" rel="stylesheet">
+</head>
+<body>
+<button onclick="login()">Ingresar</button>
+<script type="text/javascript">
+	function login(){
+		window.location.href="main.php";
+	}
+</script>
+</body>
+</html>
