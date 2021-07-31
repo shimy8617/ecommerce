7f3e9d9dc8b31f5c8bdd7a8b4abcaeaf9fdea3b1
Cristina Shim <84110240+shimy8617@users.noreply.github.com>
2021-07-31 07:27:07 -0300

    Add files via upload

---
+++ layout/_directorios.php
@@ -0,0 +1,9 @@
+<div class="body-nav-bar">
+			<div class="logo-img"><img src="assets/web/aypoo.jpg"></div>
+			<h1>Administrador</h1>
+		<ul class="mt10">
+			<li><a href="main.php">Inicio</a></li>
+			<li><a href="productos.php">Productos</a></li>
+			<li><a href="index.php">Salir</a></li>
+		</ul>
+		</div>
